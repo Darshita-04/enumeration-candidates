bb1193c56a77552cda92ceae9de5ffde60b727de
Darshita Patel <pateldarshita1994@gmail.com>
2025-03-05 04:19:51 -0500

    defined methods to fetch and filter data of candidates

--- main.rb
+++ main.rb
@@ -7,3 +7,19 @@ require './filters'
 ## Your test code can go here
 
 pp @candidates
+
+
+puts "experienced candidates:"
+puts experienced?(@candidates[0])
+
+puts "\ncandidate with ID 10:"
+candidate = find(10)
+puts candidate ? candidate : "Candidate not found"
+
+puts "\nQualified candidates:"
+qualified = qualified_candidates(@candidates)
+qualified.each { |c| puts "ID: #{c[:id]}, Experience: #{c[:years_of_experience]}, Github Points: #{c[:github_points]}" }
+
+puts "\nCandidates ordered by qualifications:"
+ordered = ordered_by_qualifications(@candidates)
+ordered.each { |c| puts "ID: #{c[:id]}, Experience: #{c[:years_of_experience]}, Github Points: #{c[:github_points]}" }
